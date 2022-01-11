433d23c9bc9295aac812bf998ba99c1d7c9186e0
WTW0313 <925448163@qq.com>
2022-01-11 14:20:59 +0800

    modify globals

--- 2-globals.js
+++ 2-globals.js
@@ -1,9 +1,10 @@
 // Globals
 
-// __dirname  -path to current directory
-// __filename -file name
-// require    -function to use modules (CommonJS)
-// module     -info about current module (file)
-// process    -info about the env where the program is being excuted
+// * __dirname  -path to current directory
+// * __filename -file name
+// * require    -function to use modules (CommonJS)
+// * module     -info about current module (file)
+// * process    -info about the env where the program is being excuted
+// * exports    -module.exports
 
 console.log(__dirname);
